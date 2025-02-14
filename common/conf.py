89d5900d346bf3e4b5b7a797dad2d3ca61527c77
git <139.9.288.28>
2025-02-14 21:59:04 +0800

    config.py

--- common/conf.py
+++ common/conf.py
@@ -8,9 +8,9 @@ from pathlib import Path
 # from common.config.main_config import setting_cfg
 WINDOWS_TITLE = 'L4D2 Mod管理器'
 
-IS_DEV = False
+IS_DEV = True
 
-VERSION = '1.2.1'
+VERSION = '1.2.2'
 if IS_DEV:
     VERSION += ' dev'
 
